44ed1b657a2cca822a05d7d5cc7027d7db2213de
Jamie Brown <jmbrowncow@gmail.com>
2018-08-08 19:20:24 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,10 +5,12 @@ class PigLatinizer
     phrase_array.collect do |word|
       if word[0].downcase.match(/[aeiou]/)
         word+"way"
-      elsif word[-2..-1] =="ay"
-        "ay"+word
-      else
+      elsif word[1].downcase.match(/[aeiou]/)
         word[1..-1]+word[0]+"ay"
+      elsif word[2].downcase.match(/[aeiou]/)
+        word[2..-1]+word[0..1]+"ay"
+      else
+        word[3..-1]+word[0..2]+"ay"
       end
       #binding.pry
     end.join(" ")
