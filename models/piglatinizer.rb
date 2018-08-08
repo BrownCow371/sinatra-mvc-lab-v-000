d63cbe493c6fd0bfce90785e1eaf8f21f7e3cd67
Jamie Brown <jmbrowncow@gmail.com>
2018-08-08 19:05:11 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,11 +1,7 @@
 class PigLatinizer
 
-  def initialize(phrase = "")
-    @phrase = phrase
-  end
-
-  def piglatinize
-    phrase_array = @phrase.split(" ")
+  def piglatinize(phrase)
+    phrase_array = phrase.split(" ")
     phrase_array.collect do |word|
       if word.start_with?(/[aeiou]/)
         word+"way"
