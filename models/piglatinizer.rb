e92ed6d5c39537c24afde22d47e091b9e4b32f18
Jamie Brown <jmbrowncow@gmail.com>
2018-08-08 19:10:31 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,7 +3,7 @@ class PigLatinizer
   def piglatinize(phrase)
     phrase_array = phrase.split(" ")
     phrase_array.collect do |word|
-      if word[0].match(/[aeiou]/)
+      if word[0].downcase.match(/[aeiou]/)
         word+"way"
       elsif word[-2..-1] =="ay"
         "ay"+word
